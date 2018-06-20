d9e691c54043e6aca4be0473f804265b60158446
prabhakaran.anbazhag <prabhakarana2018@siet.ac.in>
2018-06-20 18:55:14 +0530

    Piechart using JSON

--- app/src/main/java/com/example/prabhakarananbazhag/grapie/MainActivity.java
+++ app/src/main/java/com/example/prabhakarananbazhag/grapie/MainActivity.java
@@ -1,94 +1,25 @@
 package com.example.prabhakarananbazhag.grapie;
 
-import android.content.SharedPreferences;
-import android.content.res.AssetManager;
-import android.graphics.Canvas;
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import javax.xml.parsers.ParserConfigurationException;
-
 public class MainActivity extends AppCompatActivity {
-    //static final String NODE_CANVAS="canvas";
-//static final String NODE_TEAM="team";
-//int X,Y,Width,Height,Trans;
-   /* ArrayList<String> teamname = new ArrayList<>();
-    ArrayList<Float> matches = new ArrayList<>();
-    ArrayList<String> colour = new ArrayList<>();*/
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MyCanvas mc = (MyCanvas) findViewById(R.id.grap);
-
- /*       XMLDOMParser parser = new XMLDOMParser();
-        AssetManager manager = getAssets();
-        InputStream stream;
-        try {
-            stream = manager.open("data.xml");
-            Document doc = parser.getDocument(stream);
-
-
-            NodeList nodeList = doc.getElementsByTagName(NODE_CANVAS);
-                Element e = (Element) nodeList.item(0);
-                X=Integer.parseInt(parser.getValue(e,"value_x"));
-                Y=Integer.parseInt(parser.getValue(e,"value_y"));
-                Width=Integer.parseInt(parser.getValue(e,"width"));
-                Height=Integer.parseInt(parser.getValue(e,"height"));
-
-
-            NodeList nodeList1=doc.getElementsByTagName(NODE_TEAM);
-
-                for(int i=0;i<nodeList1.getLength();i++){
-                    Element team=(Element)nodeList1.item(i);
-                    teamname.add(parser.getValue(team,"name"));
-                    matches.add(Float.valueOf(parser.getValue(team,"matches")));
-                    colour.add(parser.getValue(team,"colour"));
-                }
-
-          NodeList nodeList2=doc.getElementsByTagName("Trans");
-        } catch (IOException e1) {
-            e1.printStackTrace();
-        }*/
-
-
-       // PieChartData obj = new PieChartData(teamname, matches, colour);
-       /*RelativeLayout relativeLayout=(RelativeLayout)findViewById(R.id.grap);
-        RelativeLayout relativeLayout=(RelativeLayout)findViewById(R.id.rel);
-        RelativeLayout.LayoutParams params=new RelativeLayout.LayoutParams(Width,Height);
-        params.leftMargin=X;
-        params.topMargin=Y;*/
-
-       /* mc.setLayoutParams(new ViewGroup.LayoutParams(Width,Height));
-        mc.setBackgroundColor(Color.GRAY);
-        relativeLayout.addView(mc,params);*/
-
-       mc.setdata(getjson());
+        Piechart mc = (Piechart) findViewById(R.id.grap);
+        mc.setdata(getjson());
     }
 
 public  ArrayList<HashMap<String, String>> getjson(){
@@ -96,21 +27,18 @@ public  ArrayList<HashMap<String, String>> getjson(){
     try {
         JSONObject obj = new JSONObject(loadJSONFromAsset());
         JSONArray m_jArry = obj.getJSONArray("team");
-         formList = new ArrayList<HashMap<String, String>>();
+        formList = new ArrayList<HashMap<String, String>>();
         HashMap<String, String> m_li;
 
         for (int i = 0; i < m_jArry.length(); i++) {
             JSONObject jo_inside = m_jArry.getJSONObject(i);
-           // Log.d("Details-->", jo_inside.getString("name"));
             String name_value = jo_inside.getString("name");
             String matches_value = jo_inside.getString("matches");
             String colour_value=jo_inside.getString("colour");
-            //Add your values in your `ArrayList` as below:
             m_li = new HashMap<String, String>();
             m_li.put("name", name_value);
             m_li.put("matches", matches_value);
             m_li.put("colour",colour_value);
-
             formList.add(m_li);
         }
     } catch (JSONException e) {
@@ -133,5 +61,4 @@ public  ArrayList<HashMap<String, String>> getjson(){
         }
         return json;
     }
-
 }
--- app/src/main/java/com/example/prabhakarananbazhag/grapie/Piechart.java
+++ app/src/main/java/com/example/prabhakarananbazhag/grapie/Piechart.java
@@ -15,119 +15,89 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class MyCanvas extends View {
+public class Piechart extends View {
     Paint paint,paint1;
     ArrayList<HashMap<String, String>> pieChartData;
     float start=0f;
-   
-
-
-    public MyCanvas(Context context){
+    public Piechart(Context context){
     super(context);
-
-
-}
-
-    public MyCanvas(Context context, @Nullable AttributeSet attrs) {
+    }
+    public Piechart(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         paint=new Paint();
         paint1=new Paint();
         paint.setColor(Color.BLACK);
     }
 
-    public MyCanvas(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    public Piechart(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
-
 @Override
     protected void onDraw(Canvas canvas) {
     super.onDraw(canvas);
     if(pieChartData!=null){
-        float[] sweep=scale();
+     float[] sweep=scale();
         int z=canvas.getWidth()/20;
+        Log.i("val", String.valueOf(getWidth()));
         int w=canvas.getWidth()/30;
         int x=canvas.getWidth()-z;
         int y= canvas.getHeight()-z;
         RectF rectF = new RectF(z,z,x,y);
         RectF rectF1=new RectF(w,w,x+w,y+w);
         for(int i=0;i<pieChartData.size();++i){
+            paint.setColor(Color.parseColor(pieChartData.get(i).get("colour")));
+            Path p2=new Path();
             canvas.drawArc(rectF, start, sweep[i], true, paint);
-            start += sweep[i];
-        }
-
-    }else{
-        return;
-    }
-
-
-   /* if (pieChartData != null) {
-            float[] sweep=scale();
-
-            int z=canvas.getWidth()/20;
-            int w=canvas.getWidth()/30;
-
-            int x=canvas.getWidth()-z;
-            int y= canvas.getHeight()-z;
-            RectF rectF = new RectF(z,z,x,y);
-            RectF rectF1=new RectF(w,w,x+w,y+w);
-
-        for(int i=0;i<pieChartData.getMatches().size();i++) {
-
-
-            paint.setColor(Color.parseColor(pieChartData.getColor().get(i)));
-
-            canvas.drawArc(rectF, start, sweep[i], true, paint);
-
-            //paint.setShadowLayer(10.0f, 6.0f, 6.0f, Color.BLACK);
-            // setLayerType(LAYER_TYPE_SOFTWARE,paint);
-
-
+            p2.addArc(rectF,start,sweep[i]);
             Path p = new Path();
             p.addArc(rectF1, start, sweep[i]);
-            PathMeasure pathMeasure = new PathMeasure(p, false);
+            PathMeasure pathMeasure = new PathMeasure(p2, false);
             float pathLength = pathMeasure.getLength();
             paint.setColor(Color.BLACK);
             paint.setTextSize(getWidth() / 25);
             paint.setTextAlign(Paint.Align.CENTER);
-            String name = pieChartData.getName().get(i);
+            String name = pieChartData.get(i).get("name");
             if (paint.measureText(name) > pathLength) {
                 while (paint.measureText(name) < pathLength) {
                     name = name.substring(0, name.length() - 1);
                 }
                 canvas.drawTextOnPath(name.substring(0, name.length() - 3).concat("..."), p, 0, 0, paint);
             } else {
-                canvas.drawTextOnPath(pieChartData.getName().get(i), p, 0, 0, paint);
+                canvas.drawTextOnPath(pieChartData.get(i).get("name"), p, 0, 0, paint);
             }
             start += sweep[i];
+//            paint1.setShadowLayer(10.0f, 6.0f, 6.0f, Color.BLACK);
+//            setLayerType(LAYER_TYPE_SOFTWARE,paint);
+
         }
-    } else {
+
+    }else{
         return;
     }
-
-*/
 }
     private float[] scale() {
-
-float[] sweep=new float[pieChartData.get(Integer.parseInt("matches")).size()];
+float[] sweep=new float[pieChartData.size()];
         float total = getTotal();
-for(int i=0;i<pieChartData.get(Integer.parseInt("matches")).size();i++){
-  //  sweep[i] = (pieChartData.add("matches",pieChartData).get(i) / total) * 360;
-}
+for(int i=0;i<pieChartData.size();i++){
+      sweep[i] = (Float.parseFloat(pieChartData.get(i).get("matches")) /total) * 360;
+     }
         return sweep;
     }
    private float getTotal(){
        float total = 0;
-      // for (float val : pieChartData.getMatches())
-         //  total += val;
- return total;
+      for(int i=0;i<pieChartData.size();i++) {
+          float val= Float.parseFloat(pieChartData.get(i).get("matches"));
+          total += val;
+      }
+      return total;
    }
     public void setdata(ArrayList<HashMap<String, String>> chartdata){
         pieChartData=chartdata;
-      postInvalidate();
+        postInvalidate();
     }
-
-
 }
